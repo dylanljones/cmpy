240a8bddda4ce079190985be3996101c74be91b6
Dylan Jones <jonesdyl@fingolfin.physik.uni-augsburg.de>
2019-08-06 16:05:44 +0200

    dmft

--- dmft.py
+++ dmft.py
@@ -13,7 +13,7 @@ from cmpy.hubbard import HubbardModel, State, Siam
 #  https://github.com/DerWeh/gftools/blob/master/gftools/__init__.py
 
 
-def bethe_gf_omega(z, half_bandwidth):
+def bethe_gf_omega(z, half_bandwidth=1):
     """Local Green's function of Bethe lattice for infinite Coordination number.
     Parameters
     ----------
@@ -32,21 +32,52 @@ def bethe_gf_omega(z, half_bandwidth):
     return 2./half_bandwidth*z_rel*(1 - np.sqrt(1 - 1/(z_rel*z_rel)))
 
 
-def main():
+
+def bath_greens_function(gf_loc, sigma):
+    gf_inv = 1/gf_loc + sigma
+    return 1/gf_inv
+
+
+def dmft():
     model = Siam(eps_d=0., u=10, eps=0., v=1., mu=0)
 
     n = 100
-    omegas = np.linspace(-5, 5, 100) + eta
+    omega = eta
     sigma = np.zeros(n)
 
-    gf_loc = bethe_gf_omega(omegas, 1)
-    delta = model.hybridization(omegas)
-    gf_imp = 1/(omegas + model.mu - model.eps_d - sigma - delta)
+    weiss = 1
+    weiss_inv = 1/weiss
+    gf_loc = bethe_gf_omega(omega)
 
-    plot = Plot()
-    plot.plot(omegas.real, gf_imp.imag)
-    plot.plot(omegas.real, gf_loc.imag)
-    plot.show()
+    for i in range(100):
+        sigma = weiss_inv - 1/gf_loc
+        gf_loc = bethe_gf_omega(omega - sigma)
+        weiss_inv = 1/gf_loc + sigma
+
+
+def hf_qmc(gf_bath, t=0.05, nt=100):
+    beta = 1/t
+    delta_tau = beta/nt
+
+
+
+
+
+def main(sigma_0=1, nt=100):
+    sigma = sigma_0
+    omega = eta
+    gf_loc = bethe_gf_omega(omega - sigma)
+    gf_bath = bath_greens_function(gf_loc, sigma)
+    hf_qmc(gf_bath)
+    print(gf_loc, gf_bath)
+
+
+
+
+
+
+
+
 
 
 if __name__ == "__main__":
