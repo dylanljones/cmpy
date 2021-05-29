4baa727c438558809702efa0a7317fdeb2ee5d5f
dylanljones <dylanljones94@gmail.com>
2021-05-29 02:16:55 +0200

    added logger

---
+++ cmpy/_utils.py
@@ -0,0 +1,32 @@
+# coding: utf-8
+#
+# This code is part of cmpy.
+# 
+# Copyright (c) 2021, Dylan Jones
+
+import logging
+from typing import Union, Iterable
+import numpy as np
+
+# define type for numpy `array_like` types
+ArrayLike = Union[int, float, Iterable, np.ndarray]
+
+# =========================================================================
+# LOGGING
+# =========================================================================
+
+# Configure package logger
+logger = logging.getLogger("cmpy")
+
+_stream_handler = logging.StreamHandler()
+_stream_handler.setLevel(logging.DEBUG)
+
+# _frmt_str = "[%(asctime)s] %(levelname)-8s - %(name)s - %(funcName)s - %(message)s"
+_frmt_str = "[%(asctime)s] %(name)s:%(levelname)-8s - %(message)s"
+_formatter = logging.Formatter(_frmt_str, datefmt='%H:%M:%S')
+
+_stream_handler.setFormatter(_formatter)    # Add formatter to stream handler
+logger.addHandler(_stream_handler)          # Add stream handler to package logger
+
+logger.setLevel(logging.WARNING)            # Set initial logging level
+logging.root.setLevel(logging.NOTSET)
