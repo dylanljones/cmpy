e176f0dbe9f7abcccb1d1c07ad52b29be9a42ba7
dylanljones <dylanljones94@gmail.com>
2021-05-31 01:07:45 +0200

    Updated examples

--- lecture/gf_lehmann.py
+++ lecture/gf_lehmann.py
@@ -7,7 +7,6 @@
 import logging
 import numpy as np
 import matplotlib.pyplot as plt
-from cmpy import UP
 from cmpy.models import SingleImpurityAndersonModel
 from cmpy import exactdiag as ed
 
@@ -15,43 +14,17 @@ logger = logging.getLogger("cmpy")
 logger.setLevel(logging.DEBUG)
 
 
-def greens_function_lehmann(model, z, beta, pos=0, sigma=UP):
-    data = ed.GreensFunctionMeasurement(z, beta, pos, sigma)
-    eig_cache = dict()
-    occ = 0.
-    occ_double = 0.
-    for n_up, n_dn in model.iter_fillings():
-        sector = model.get_sector(n_up, n_dn)
-        eigvals, eigvecs = ed.solve_sector(model, sector, cache=eig_cache)
-
-        occ += ed.occupation(sector, eigvals, eigvecs, beta, pos=pos, sigma=sigma)
-        occ_double += ed.double_occupation(sector, eigvals, eigvecs, beta, pos=pos)
-        # Check if upper particle sector exists
-        sector_p1 = model.basis.upper_sector(n_up, n_dn, sigma)
-        if sector_p1 is not None:
-            eigvals_p1, eigvecs_p1 = ed.solve_sector(model, sector_p1, cache=eig_cache)
-            data.accumulate(sector, sector_p1, eigvals, eigvecs, eigvals_p1, eigvecs_p1)
-        else:
-            eig_cache.clear()
-
-    print(f"Occ-up {occ / data.part:.6f}")
-    print(f"Occ-up {data.occ:.6f}")
-    print(f"double occ-up {occ_double / data.part:.6f}")
-    print(f"double occ-up {data.occ_double:.6f}")
-    return data.gf
-
-
 def main():
     beta = 100
-    num_bath = 1
+    num_bath = 4
     u = 2
     eps_imp = 0
-    eps_bath = 0 * np.random.uniform(size=num_bath)
+    eps_bath = 1 * np.random.uniform(size=num_bath)
     v = np.ones(num_bath)
 
     siam = SingleImpurityAndersonModel(u, eps_imp, eps_bath, v, mu=None)
 
-    z = np.linspace(-7, +7, 1000) + 0.01j
+    z = np.linspace(-10, +10, 1000) + 0.01j
     data = ed.greens_function_lehmann(siam, z, beta)
     gf = data.gf
 
--- lecture/gf_tevo.py
+++ lecture/gf_tevo.py
@@ -15,26 +15,24 @@ logger.setLevel(logging.DEBUG)
 
 
 def main():
-    tmax, dt = 300, 0.1
-    start, stop, num = 0, tmax, int(tmax / dt)
-    num_bath = 1
-    u = 2
+    num_bath = 2
+    u = 5
     eps_imp = 0
-    eps_bath = 0 * np.random.uniform(size=num_bath)
+    eps_bath = 1 * np.random.uniform(size=num_bath)
     v = np.ones(num_bath)
-
     siam = SingleImpurityAndersonModel(u, eps_imp, eps_bath, v, mu=None)
 
-    times, gf_t = ed.greens_function_tevo(siam, start, stop, num)
+    start, stop, dt = 0, 150, 0.05
+    times, gf_t = ed.greens_function_tevo(siam, start, stop, num=int(stop / dt) + 1)
 
     fig, ax = plt.subplots()
     ax.plot(times, -gf_t.imag)
     ax.set_xlim(times[0], times[-1])
-    ax.set_ylim(-1.1, +1.1)
+    # ax.set_ylim(-1.1, +1.1)
     ax.grid()
 
     w = np.linspace(-7, +7, 5000)
-    z, gf_w = ed.fourier_t2z(times, gf_t, w, delta=1e-2)
+    z, gf_w = ed.fourier_t2z(times, gf_t, w, delta=1e-4)
 
     fig, ax = plt.subplots()
     ax.plot(z.real, -gf_w.imag)
