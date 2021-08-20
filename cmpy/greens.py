4f3655063ed1314dfb820780f733f9598250fdd7
dylanljones <dylanljones94@gmail.com>
2021-08-20 09:31:53 +0200

    added common GF's

--- cmpy/greens.py
+++ cmpy/greens.py
@@ -12,28 +12,30 @@
 
 import numpy as np
 import numpy.linalg as la
-from .operators import CreationOperator
+from mpmath import fp
+from functools import partial
+from typing import Union
+
+_ellipk_z = np.frompyfunc(partial(fp.ellipf, np.pi/2), 1, 1)
 
 
-def gf0_lehmann(ham, omega, mu=0., only_diag=True):
-    """ Calculate the greens function of the given Hamiltonian
+def gf0_lehmann(ham: np.ndarray, z: Union[complex, np.ndarray],
+                mu: float = 0., only_diag: bool = True) -> np.ndarray:
+    """Calculate the non-interacting Green's function.
 
     Parameters
     ----------
-    ham: array_like
-        Hamiltonian matrix
-    omega: complex or array_like
-        Energy e+eta of the greens function (must be complex!)
-    mu: float, default: 0
-        Chemical potential of the system
-    only_diag: bool, default: True
-        only return diagonal elements of the greens function if True
+    ham : (N, N) np.ndarray
+        Array representing the Hamiltonian matrix.
+    z : (..., Nw) complex np.ndarray or complex
+        Green's function is evaluated at complex frequency `z`.
+    mu : float, optional
+        Chemical potential of the system.
+    only_diag : bool, optional
+        Only return diagonal elements of the greens function if `True`.
 
-    Returns
-    -------
-    greens: np.ndarray
     """
-    omega = np.atleast_1d(omega)
+    z = np.atleast_1d(z)
 
     # Calculate eigenvalues and -vectors of hamiltonian
     eigvals, eigvecs = np.linalg.eigh(ham)
@@ -41,61 +43,95 @@ def gf0_lehmann(ham, omega, mu=0., only_diag=True):
 
     # Calculate greens-function
     subscript_str = "ij,...j,ji->...i" if only_diag else "ik,...k,kj->...ij"
-    arg = np.subtract.outer(omega + mu, eigvals)
+    arg = np.subtract.outer(z + mu, eigvals)
     greens = np.einsum(subscript_str, eigenvectors_adj, 1 / arg, eigvecs)
     return greens
 
 
-class GreensFunction(np.ndarray):
-    """Container class for the single-particle interacting Greens function."""
+# =========================================================================
+# Common Green's functions
+# =========================================================================
 
-    def __new__(cls, z, beta, dtype=None):
-        r"""Create new `GreensFunction`` instance in the shape of `z`.
 
-        Parameters
-        ----------
-        z : (N) np.ndarray
-            The broadened complex frequency .math:`\omega + i \eta`.
-        beta : float
-            The inverse temperature .math:`\beta = 1 / T`.
-        dtype : str or int or np.dtype, optional
-            Optional datatype of the gf-array. The default is the same as `z`.
-        """
-        inputarr = np.zeros_like(z)
-        self = np.asarray(inputarr, dtype).view(cls)
-        self.z = z
-        self.beta = beta
-        return self
+def _u_ellipk(z):
+    """Complete elliptic integral of first kind `scipy.special.ellip` for complex arguments.
 
-    def accumulate(self, cdag, eigvals, eigvecs, eigvals_p1, eigvecs_p1, min_energy=0.):
-        """Accumulate the `GreensFunction` iterative.
+    Wraps the `mpmath` implementation `mpmath.fp.ellipf` using `numpy.frompyfunc`.
 
-        Parameters
-        ----------
-        cdag : CreationOperator
-            The creation operator used in the expectation .math:`<n|c^†|m>`.
-        eigvals : (N) np.ndarray
-            The eigenvalues of the right state .math:`|m>`.
-        eigvecs : (N, N) np.ndarray
-            The eigenvectors of the right state .math:`|m>`.
-        eigvals_p1 : (M) np.ndarray
-            The eigenvalues of the left state .math:`<n|`.
-        eigvecs_p1 : (M) np.ndarray
-            The eigenvectors of the left state .math:`<n|`.
-        min_energy : float, optional
-            The ground-state energy. The default is ``0``-
-        """
-        cdag_vec = cdag.matmat(eigvecs)
-        overlap = abs(eigvecs_p1.T.conj() @ cdag_vec) ** 2
+    Parameters
+    ----------
+    z : complex or complex array_like
+        Complex argument
+    Returns
+    -------
+    complex np.ndarray or complex
+        The complete elliptic integral.
+    """
+    ellipk = _ellipk_z(np.asarray(z, dtype=complex))
+    try:
+        ellipk = ellipk.astype(complex)
+    except AttributeError:  # complex not np.ndarray
+        pass
+    return ellipk
 
-        if np.isfinite(self.beta):
-            exp_eigvals = np.exp(-self.beta * (eigvals - min_energy))
-            exp_eigvals_p1 = np.exp(-self.beta * (eigvals_p1 - min_energy))
-        else:
-            exp_eigvals = np.ones_like(eigvals)
-            exp_eigvals_p1 = np.ones_like(eigvals_p1)
 
-        for m, eig_m in enumerate(eigvals_p1):
-            for n, eig_n in enumerate(eigvals):
-                weights = exp_eigvals[n] + exp_eigvals_p1[m]
-                self[:] += overlap[m, n] * weights / (self.z + eig_n - eig_m)
+def gf_z_bethe(z: Union[complex, np.ndarray], half_bandwidth: float) -> Union[float, np.ndarray]:
+    r"""Local Green's function of the Bethe lattice for infinite coordination number.
+
+    Parameters
+    ----------
+    z : (..., Nw) complex np.ndarray or complex
+        Green's function is evaluated at complex frequency `z`.
+    half_bandwidth : float
+        Half-bandwidth of the DOS of the 1D lattice.
+        Corresponds to the nearest neighbor hopping `t=D/2`
+
+    Returns
+    -------
+    gf : (..., Nw) complex np.ndarray or complex
+        The local Green's function.
+    """
+    dtype = np.complex256
+    z_rel = np.array(z / half_bandwidth, dtype=dtype)
+    gf = 2. / half_bandwidth * z_rel * (1 - np.sqrt(1 - z_rel**(-2)))
+    return gf.astype(dtype=dtype, copy=False)
+
+
+def gf_z_onedim(z: Union[complex, np.ndarray], half_bandwidth: float) -> Union[float, np.ndarray]:
+    """Local Green's function of the 1D lattice.
+
+    Parameters
+    ----------
+    z : (..., Nw) complex np.ndarray or complex
+        Green's function is evaluated at complex frequency `z`.
+    half_bandwidth : float
+        Half-bandwidth of the DOS of the 1D lattice.
+        Corresponds to the nearest neighbor hopping `t=D/2`
+
+    Returns
+    -------
+    gf : (..., Nw) complex np.ndarray or complex
+        The local Green's function.
+    """
+    return 1. / (z * np.lib.scimath.sqrt(1 - (half_bandwidth / z) ** 2))
+
+
+def gf_z_square(z: Union[complex, np.ndarray], half_bandwidth: float) -> Union[float, np.ndarray]:
+    """Local Green's function of the square lattice.
+
+    Parameters
+    ----------
+    z : (..., Nw) complex np.ndarray or complex
+        Green's function is evaluated at complex frequency `z`.
+    half_bandwidth : float
+        Half-bandwidth of the DOS of the square lattice.
+        Corresponds to the nearest neighbor hopping `t=D/2`
+
+    Returns
+    -------
+    gf : (..., Nw) complex np.ndarray or complex
+        The local Green's function.
+    """
+    z_rel_inv = half_bandwidth/z
+    elliptic = _u_ellipk(z_rel_inv**2)
+    return 2./np.pi/half_bandwidth*z_rel_inv*elliptic
