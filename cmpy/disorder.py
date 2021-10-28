36792092b437f472707ba9c788ecea1825053b5f
dylanljones <dylanljones94@gmail.com>
2021-10-28 15:14:15 +0200

    updated disorder methods

--- cmpy/disorder.py
+++ cmpy/disorder.py
@@ -15,7 +15,7 @@ from typing import Union, Sequence
 
 
 def split_num(number: int, fractions: Union[float, Sequence[float]]) -> np.ndarray:
-    """Splits an integer into fractions.
+    """Splits an integer into integer fractions.
 
     Parameters
     ----------
@@ -38,8 +38,8 @@ def split_num(number: int, fractions: Union[float, Sequence[float]]) -> np.ndarr
     return parts
 
 
-def create_substitutional_array(size: int, conc: Union[float, Sequence[float]],
-                                values: Sequence[float]) -> np.ndarray:
+def create_subst_array(size: int, conc: Union[float, Sequence[float]],
+                       values: Sequence[float]) -> np.ndarray:
     """Creates an (ordered) array of values.
 
     Parameters
@@ -64,6 +64,32 @@ def create_substitutional_array(size: int, conc: Union[float, Sequence[float]],
 
 
 def random_permutations(arr, size, replace=False):
+    """Creates (optionally unique) permutations of a given array.
+
+    Parameters
+    ----------
+    arr : (N) np.ndarray
+        The input array to permute.
+    size : int
+        The number of permutations to generate.
+    replace : bool, optional
+        If `True`, only unique permutations are returned. The default is `True`.
+
+    Yields
+    ------
+    perm : (N) np.ndarray
+        The permuted array.
+
+    Examples
+    --------
+    >>> np.random.seed(0)
+    >>> a = np.array([0, 0, 1, 1, 1])
+    >>> perm = random_permutations(a, 2)
+    >>> next(perm)
+    array([1, 0, 0, 1, 1])
+    >>> next(perm)
+    array([0, 1, 1, 1, 0])
+    """
     p = arr.copy()
     seen = set()
     count = 0
@@ -80,3 +106,9 @@ def random_permutations(arr, size, replace=False):
         else:
             yield p
             count += 1
+
+
+def subst_disorder_iter(size: int, conc: Union[float, Sequence[float]],
+                        values: Sequence[float], samples: int):
+    ordered = create_subst_array(size, conc, values)
+    return random_permutations(ordered, samples)
